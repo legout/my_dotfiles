ebf0f09135d2cbe3970ae8f586647766f818e30e
legout <ligno.blades@gmail.com>
2023-10-10 08:52:15 +0200

    init config files repo

---
+++ .config/nvim/init.lua
@@ -0,0 +1,25 @@
+-- load options
+require("options")
+
+-- load keymappings
+require("keymaps")
+
+-- load Packervim
+require("plugins")
+
+-- Set colorscheme
+require("colorscheme")
+
+-- Set LSP
+require("lsp")
+
+-- config
+--require("config/bufferline")
+--require("config/gitsigns")
+--require("config/indent-blankline")
+--require("config/lualine")
+-- require('config/mason-null-ls')
+--require('config/nvim-cmp')
+--require("config/nvim-tree")
+--require("config/nvim-treesitter")
+--require("config/toggleterm")
